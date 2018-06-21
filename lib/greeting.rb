f0a72ee84849072910efb7e1a6cc7b827e16bfe8
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-21 08:47:23 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
-def greeting()
-  puts "Make sure bin/greet has code that can output 'Hi! I'm HAL, what's your name?' exactly."
+def greeting(name)
+  puts "Hi! I'm HAL, what's your name?' exactly."
   name = gets.strip
   puts "Hello #{name}. It's nice to meet you."
 end
